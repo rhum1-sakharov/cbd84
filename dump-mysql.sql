deca5fd67dfa5f6051c31d7563d563c59294ba1e
romain.vermorel <romain.vermorel@gmail.com>
2015-11-27 17:56:43 +0100

    chore : save mysql dump

--- dump-mysql.sql
+++ dump-mysql.sql
@@ -37,8 +37,11 @@ CREATE TABLE `contacts` (
   `photoUrl` varchar(255) DEFAULT NULL,
   `position` int(11) DEFAULT NULL,
   `postalCode` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `assoCodeLabel` varchar(255) DEFAULT NULL,
+  `assoName` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_contact`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +50,7 @@ CREATE TABLE `contacts` (
 
 LOCK TABLES `contacts` WRITE;
 /*!40000 ALTER TABLE `contacts` DISABLE KEYS */;
-INSERT INTO `contacts` VALUES (1,'15 rue des Tours','VALREAS','vich.raymond@neuf.fr','Raymond','Pr&#233;sident','Vich','06 87 11 19 45 ','04 90 35 28 38','images/get/contacts/jpg/1',1,'84600'),(2,'493 Avenue Marius Coulon','BOLLENE','pyrame@gmail.com','Gilbert','Secr&#233;taire G&#233;n&#233;ral','Navarro','06 87 80 59 46 ',NULL,'images/get/contacts/jpg/2',2,'84500'),(3,'Chemin des N&#233gades','ORANGE','negades.alain@wanadoo.fr','Alain','Tr&#233;sorier','Canonge','','04 90 34 98 39 ','images/get/contacts/jpg/3',3,'84100');
+INSERT INTO `contacts` VALUES (1,'15 rue des Tours','VALREAS','vich.raymond@neuf.fr','Raymond','Président','Vich','06 87 11 19 45 ','04 90 35 28 38','images/get/contacts/jpg/1',1,'84600','person',NULL,NULL),(2,'493 Avenue Marius Coulon','BOLLENE','pyrame@gmail.com','Gilbert','Secr&#233;taire G&#233;n&#233;ral','Navarro','06 87 80 59 46 ',NULL,'images/get/contacts/jpg/2',2,'84500','person',NULL,NULL),(3,'Chemin des N&#233gades','ORANGE','negades.alain@wanadoo.fr','Alain','Trésorier','Canonge','','04 90 34 98 39 ','images/get/contacts/jpg/3',3,'84100','person',NULL,NULL),(12,'693, route d\'Aubignan','SARRIANS','gemo.fadat@laposte.net','Gérald','Président','Fadat','06 82 59 62 99','04 90 60 59 97','images/get/contacts/jpg/12?cb=1448358729120?cb=1448358754440?cb=1448358829898',1,'84260','asso','Pro 84 01 17','Boule Sarriannaise'),(13,'2504, Avenue de Pernes','MAZAN','','Dany','Président','Bertrand',NULL,'04 90 35 28 38','images/get/contacts/jpg/13',2,'84380','asso','Pro 84 01 18','Amicale boule Sorguaise'),(17,'268 grand rue','saint gely du fesc','romain.vermorel@gmail.com','Romain','IT and Communication Manager','Vermorel',NULL,NULL,'images/get/contacts/jpg/17',4,'34980','person',NULL,NULL);
 /*!40000 ALTER TABLE `contacts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -70,8 +73,9 @@ CREATE TABLE `feeds` (
   `imageUrl` varchar(255) DEFAULT NULL,
   `imageTitle` varchar(255) DEFAULT NULL,
   `imageExtension` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_feed`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -80,7 +84,7 @@ CREATE TABLE `feeds` (
 
 LOCK TABLES `feeds` WRITE;
 /*!40000 ALTER TABLE `feeds` DISABLE KEYS */;
-INSERT INTO `feeds` VALUES (2,'Raymond Vich','<p>J\'ai voulu cr&eacute;er ce blog pour faire <strong>connaitre</strong> notre passion qui est le sport boule lyonnaise. Grace &agrave; nos b&eacute;n&eacute;voles, notre association organise des concours et des repas dans la convivialit&eacute; avec toutes les personnes qui l\'entoure dans une bonne ambiance.</p>\n<p>Je vous rappelle la reu de vendredie prochain</p>','2015-09-16 00:00:00','Le mot du président','\0','','left',NULL,'Concours d\'Avoriaz',NULL),(3,'Alain Canonge','<p>Depechez vous de venir vous inscrire au concours d\'Orange. Beaucoup de lots &agrave; gagner et super ambiance assur&eacute;e !!</p>','2015-10-27 00:00:00','Inscription au concours d\'orange','\0','\0','left','feeds/3/image/jpg','Inscriptions','jpg'),(4,'Romain Vermorel','<p>Consultez les actus du CBD Vaucluse sur votre t&eacute;l&eacute; portable ;)</p>','2015-09-15 00:00:00','Le site est compatible avec les smartphones !!!','\0','\0','right','feeds/4/image/jpg','Smartphone','jpg');
+INSERT INTO `feeds` VALUES (2,'Raymond Vich','<p>J\'ai voulu cr&eacute;er ce blog pour faire <strong>connaitre</strong> notre passion qui est le sport boule lyonnaise. Grace &agrave; nos b&eacute;n&eacute;voles, notre association organise des concours et des repas dans la convivialit&eacute; avec toutes les personnes qui l\'entoure dans une bonne ambiance.</p>\n<p>Je vous rappelle la reu de vendredie prochain</p>','2015-11-27 11:57:35','Le mot du président','','','right','images/get/feeds/jpg/2?cb=984191.41855849071','Concours d\'Avoriaz','jpg','actus'),(3,'Alain Canonge','<p>Depechez vous de venir vous inscrire au concours d\'Orange. Beaucoup de lots &agrave; gagner et super ambiance assur&eacute;e !!</p>','2015-10-27 00:00:00','Inscription au concours d\'orange','','\0','left','images/get/feeds/jpg/3','Inscriptions','jpg','actus'),(4,'Romain Vermorel','<p>Consultez les actus du CBD Vaucluse sur votre t&eacute;l&eacute; portable ;)</p>','2015-09-15 00:00:00','Le site est compatible avec les smartphones !!!','\0','\0','right','images/get/feeds/jpg/4','Smartphone','jpg','actus'),(34,'Romain Vermorel','<p>Bonjour,</p>\n<p>&nbsp;</p>\n<p>Bienvenue dans le nouveau site du CBD Vaucluse</p>','2015-08-09 12:48:43','Une actu en or','\0','\0',NULL,NULL,NULL,NULL,'actus'),(52,'sdqds','<p>sqdsqd</p>','2015-11-27 15:59:46','dqsdsq','\0','\0',NULL,NULL,'sds',NULL,'actus'),(54,'fgdfg','<p>fdgfdgfgfg</p>','2015-11-27 17:26:38','triplette 2015','\0','','right','images/get/feeds/jpg/54?cb=974672.76292419721','fgdfgfd',NULL,'results'),(72,'8','<p>8</p>','2015-11-27 17:53:11','8','\0','\0','right','images/get/feeds/jpg/72?cb=613037.02478167641',NULL,NULL,'results');
 /*!40000 ALTER TABLE `feeds` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -148,4 +152,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-23 22:31:12
+-- Dump completed on 2015-11-27 17:55:29
