38759df81a1f0e969d9b2949a9752e608408eaa5
Romain Vermorel <Romain Vermorel>
2016-06-26 18:06:30 +0200

    fix (jpg) : image not loading

--- src/main/resources/public-web-resources/js/custom/front/app-front.js
+++ src/main/resources/public-web-resources/js/custom/front/app-front.js
@@ -1,1 +1,5 @@
-angular.module('app-front', [   'cbd.front.config','angular.filter']);
+angular.module('app-front', [   'cbd.front.config','angular.filter'])
+    .config(function($compileProvider){
+        $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel|app):/);
+    })
+;
