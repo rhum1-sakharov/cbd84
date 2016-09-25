80badf1b46805d7e663c5c1fbe7f13a138dc0967
kwkf2076 <romain.vermorel@acelys.fr>
2016-09-25 19:52:01 +0200

    change context for reverse proxy to cbd84

--- src/main/java/org/rvermorel/cbd/config/security/SecurityConfig.java
+++ src/main/java/org/rvermorel/cbd/config/security/SecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.web.header.writers.StaticHeadersWriter;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    CustomSuccessHandler customSuccessHandler;
+
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("admin").password("cbd84_rvh").roles("ADMIN");
     }
@@ -19,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests().antMatchers("/", "/front","/images/**","/feeds","/rankings","/partners","/cbdfiles","/assos").permitAll().antMatchers("/back","/back/**","/**/add**","/**/delete**","/**/update**")
-                .access("hasRole('ADMIN')").and().formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password").and().exceptionHandling()
+                .access("hasRole('ADMIN')").and().formLogin().loginPage("/login").successHandler(customSuccessHandler).usernameParameter("username").passwordParameter("password").and().exceptionHandling()
                 .accessDeniedPage("/accessDenied");
        http.csrf().disable();     
 	   http.headers().disable();
